bfed3e820e8f03a98b7c11ed57d1e18c127f21a5
tengku m risky <tengku08@gmail.com>
2020-01-14 00:26:24 +0700

    baru

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -3,13 +3,74 @@ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,Bli
 Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
+var ctx = document.getElementById("myPieChart2");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Laki-Laki", "Wanita"],
+    datasets: [{
+      data: [22, 10],
+      backgroundColor: ['#4e73df', '#FF7C7C'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673'],
+      hoverBorderColor: "rgba(234, 236)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+var ctx = document.getElementById("myPieChart1");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["K. Kantor", "K. Pemupukan", "K. Panen","K. Perawatan	"],
+    datasets: [{
+      data: [6, 6, 14,6],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc','#FF7C7C'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#36b9cc'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Staff", "KHT", "KHL"],
     datasets: [{
-      data: [55, 30, 15],
+      data: [9, 13, 10],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
